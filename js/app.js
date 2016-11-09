a9de4a8fd52ec4578dd6b1fded0af03af979befd
aijazkhan81 <aijazkhan81@gmail.com>
2016-11-09 00:41:48 -0600

    added enter functionality v0.6

--- js/app.js
+++ js/app.js
@@ -1,54 +1,66 @@
-var zero = 0;
-// $('#number').val(zero);
-$('#counter').val(zero);
-var show = $('#show');
-var reset = $('#reset');
-var toe = $('#toe');
-var finger = $('#finger');
-var countUp = null;
+var zero = 0,
+	start = $('#start'),
+	reset = $('#reset'),
+	toe = $('#toe'),
+	finger = $('#finger'),
+	countUp = null,
+	bnum = $('#number'),
+	cntr = $('#counter');
+cntr.val(zero);
 
 function runCounter(){
-    // $('#counter').val(zero);
     var run = function(){
-        var number = Number($('#number').val());
-        var counter = Number($('#counter').val());
-        if(counter < number) {
-        	show.text("Restart");	
-            counter++;
-            $('#counter').val(counter);
-            if(counter % 3 === 0){
-            	finger.addClass('highlight');
-            }
-            else{
-            	finger.removeClass('highlight');
-            };
-            if(counter % 5 === 0){
-            	toe.addClass('highlight');
-            }
-            else{
-            	toe.removeClass('highlight');
-            };
-            if((counter % 3 === 0) && (counter % 5 === 0)){
-            	toe.addClass('highlight');
-            	finger.addClass('highlight');
-            }
-        } else {
-            clearInterval(countUp);
-            finger.removeClass('highlight');
-            toe.removeClass('highlight');
-        }
+    	var number = Number(bnum.val());
+    	var counter = Number(cntr.val());
+    	if(counter < number) {
+    		start.text("Restart");	
+    		counter++;
+    		cntr.val(counter);
+    		if(counter % 3 === 0){
+    			finger.addClass('highlight');
+    		}
+    		else{
+    			finger.removeClass('highlight');
+    		};
+    		if(counter % 5 === 0){
+    			toe.addClass('highlight');
+    		}
+    		else{
+    			toe.removeClass('highlight');
+    		};
+    		if((counter % 3 === 0) && (counter % 5 === 0)){
+    			toe.addClass('highlight');
+    			finger.addClass('highlight');
+    		}
+    	} else {
+    		clearInterval(countUp);
+    		finger.removeClass('highlight');
+    		toe.removeClass('highlight');
+    	}
     };
     
     if (countUp != null) {
-        clearInterval(countUp);
+    	clearInterval(countUp);
     };
     countUp = setInterval(run, 1000);
 };
 
-show.click(runCounter);
+start.click(runCounter);
 
 reset.click(function(){
-    $('#number').val(zero);
-    $('#counter').val(zero);
-    show.text("Start");
+	bnum.val(zero);
+	cntr.val(zero);
+	start.text("Start");
+});
+
+
+bnum.keydown(function(e) {
+	if (e.which == 13) {
+		runCounter();
+		this.blur();
+		start.addClass('highlight');
+		setTimeout(function(){
+			start.removeClass('highlight');
+		}, 300)
+}
 });
