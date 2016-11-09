cee4466e8018dc723de9c585026e0441eb2bb5a7
aijazkhan81 <aijazkhan81@gmail.com>
2016-11-08 18:08:37 -0600

    v0.1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-*{
-	font-family: 'Open Sans', sans-serif;
-}
+var initialNumber = 10;
+var zero = 0;
+$('#number').val(initialNumber);
+$('#counter').val(zero);
+var number = Number($('#number').val());
+var counter = Number($('#counter').val());
+var show = $('#show');
+
+console.log('initial value of number is ' + number);
+console.log(typeof number);
+console.log('initial value of counter is ' + counter);
+
+show.click(function(){
+	var varName = function(){
+		if(counter < number) {
+			counter++;
+			console.log(counter);
+			$('#counter').val(counter);
+		} else {
+			clearInterval(varName);
+		}
+	};
+	setInterval(varName, 500);
+});
